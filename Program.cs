cd123fe22cd825972a184bbca5164fa2374c5b47
Parker Sims <parker4al@gmail.com>
2025-04-07 19:28:22 -0500

    Added methods for finding area of circle and triangle and called respected methods.

--- Program.cs
+++ Program.cs
@@ -2,8 +2,42 @@
 
 class Program
 {
+
+    public static double FindAreaOfCircle(double radius)
+    {
+
+        return Math.PI * Math.Pow(radius, 2);
+
+    }
+
+    public static double FindAreaOfTriangle(double baseLength, double height)
+    {
+
+        return baseLength * height / 2;
+
+    }
+
+    public static double FindTheAreaOfRectangle(double length, double height)
+    {
+
+        return length * height;
+
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        Console.WriteLine("What is the radius of your circle?");
+        double radius = double.Parse(Console.ReadLine());
+        Console.WriteLine($"The area of your circle is {FindAreaOfCircle(radius)}.");
+        
+        Console.WriteLine("What is the height of your triangle?");
+        double height = double.Parse(Console.ReadLine());
+        Console.WriteLine("What is the length of the base of your triangle?");
+        double baseLength = double.Parse(Console.ReadLine());
+        Console.WriteLine($"The area of your triangle is {FindAreaOfTriangle(baseLength, height)}.");
+        
+        
+
     }
 }
